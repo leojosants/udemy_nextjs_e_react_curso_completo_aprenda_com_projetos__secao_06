28ff3819ecd377c759d71f8618a4f35c15d40a27
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-11 19:29:58 -0300

    implemented the 'ResponseModel' class structure

---
+++ model/ResponseModel.ts
@@ -0,0 +1,28 @@
+export default class ResponseModel {
+    #value: string;
+    #right: boolean;
+    #revealed: boolean;
+
+    constructor(
+        value: string,
+        right: boolean,
+        revealed = false,
+    ) {
+        this.#value = value;
+        this.#right = right;
+        this.#revealed = revealed;
+    };
+
+    get value() {
+        return this.#value;
+    };
+
+    get right() {
+        return this.#right;
+    };
+
+    get revealed() {
+        return this.#revealed;
+    };
+
+};
