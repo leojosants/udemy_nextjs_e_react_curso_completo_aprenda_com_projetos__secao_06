f2d890d631644e3e84b99d453d16d75d75ac51c3
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-12 18:07:58 -0300

    Implemented 'index' component - third part

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,21 @@
 import { useEffect, useState } from 'react';
 import QuestionModel from '../model/QuestionModel';
-import ResponseModel from '../model/AnswerModel';
 import Quiz from '../components/QuizComponent';
+import { useRouter } from 'next/router';
 
-const questionMock = new QuestionModel(1, 'Qual é a sua cor favorita?',
-  [
-    ResponseModel.wrong('Verde'),
-    ResponseModel.wrong('Vermelha'),
-    ResponseModel.wrong('Azul'),
-    ResponseModel.right('Preta'),
-  ],
-);
 
 const BASE_URL = 'http://localhost:3000/api';
 
 export default function Home() {
+  const router = useRouter();
+
   const [questionsIds, setQuestionsIds] = useState<number[]>([]);
-  const [question, setQuestion] = useState<QuestionModel>(questionMock);
+  const [question, setQuestion] = useState<QuestionModel>();
   const [anwersRight, setAnwersRight] = useState<number>(0);
 
   async function loadQuestionIds() {
     const resp = await fetch(`${BASE_URL}/quiz`);
     const questionsIds = await resp.json();
-    console.log(questionsIds);
     setQuestionsIds(questionsIds);
   };
 
@@ -47,12 +40,36 @@ export default function Home() {
     setAnwersRight(anwersRight + (hit ? 1 : 0));
   };
 
-  function goToNextStep() { };
+  function nextQuestionId() {
+    if (question) {
+      const nextIndice = questionsIds.indexOf(question.id) + 1;
+      return questionsIds[nextIndice];
+    }
+  };
+
+  function goToNextStep() {
+    const nextId = nextQuestionId();
+    nextId ? goToNextQuestion(nextId) : finalize();
+  };
+
+  function goToNextQuestion(nextId: number) {
+    loadQuestion(nextId);
+  };
+
+  function finalize() {
+    router.push({
+      pathname: 'result',
+      query: {
+        total: questionsIds.length,
+        right: anwersRight,
+      }
+    });
+  };
 
   return (
     <Quiz
       question={question}
-      lastQuestion={false}
+      lastQuestion={nextQuestionId() === undefined}
       questionAnswered={questionAnswered}
       goToNextStep={goToNextStep}
     />
