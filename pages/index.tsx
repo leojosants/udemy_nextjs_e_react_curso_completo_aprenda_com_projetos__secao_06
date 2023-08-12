e754ca17acf4e84bcfb0c979bba5e9facaafc566
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-12 15:56:11 -0300

    implemented 'Index' component - first part

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import QuestionModel from '../model/QuestionModel';
 import ResponseModel from '../model/AnswerModel';
 import Quiz from '../components/QuizComponent';
@@ -12,28 +12,43 @@ const questionMock = new QuestionModel(1, 'Qual é a sua cor favorita?',
   ],
 );
 
+const BASE_URL = 'http://localhost:3000/api';
+
 export default function Home() {
-  const [question, setQuestion] = useState(questionMock);
+  const [questionsIds, setQuestionsIds] = useState<number[]>([]);
+  const [question, setQuestion] = useState<QuestionModel>(questionMock);
+
+  async function loadQuestionIds() {
+    const resp = await fetch(`${BASE_URL}/quiz`);
+    const questionsIds = await resp.json();
+    console.log(questionsIds);
+    setQuestionsIds(questionsIds);
+  };
+
+  async function loadQuestion(questionId: number) {
+    const resp = await fetch(`${BASE_URL}/questions/${questionId}`);
+    const json = await resp.json();
+    console.log(json);
+  };
+
+  useEffect(() => {
+    loadQuestionIds();
+  }, []);
+
+  useEffect(() => {
+    questionsIds.length > 0 && loadQuestion(questionsIds[0])
+  }, [questionsIds]);
 
   function questionAnswered(question: QuestionModel) { };
 
   function goToNextStep() { };
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-
-      <Quiz
-        question={question}
-        lastQuestion={false}
-        questionAnswered={questionAnswered}
-        goToNextStep={goToNextStep}
-      />
-    </div>
+    <Quiz
+      question={question}
+      lastQuestion={false}
+      questionAnswered={questionAnswered}
+      goToNextStep={goToNextStep}
+    />
   );
 };
