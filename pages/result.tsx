bf3dafde58354d9675ee5327e90dc1c84268e50c
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-12 18:47:52 -0300

    Implemented the visualization of the results of the questionnaire

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,7 +1,18 @@
+import styles from '../styles/Result.module.css';
+import { useRouter } from 'next/router';
+
 export default function Result() {
+    const router = useRouter();
+    const total = +router.query.total;
+    const right = +router.query.right;
+    const percentage = Math.round((right / total) * 100);
+
     return (
-        <div>
-            <h1>Resultado</h1>
+        <div className={styles.result}>
+            <h1>Resultado Final</h1>
+            <div>{total}</div>
+            <div>{right}</div>
+            <div>{`${percentage}%`}</div>
         </div>
-    )
-}
+    );
+};
